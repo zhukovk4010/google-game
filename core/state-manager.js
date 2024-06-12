ad18a5c805a471319037cf1a16205f26315e22b5
Zhukov Kirill <zhukovk4010@gmail.com>
2024-06-12 21:52:53 +0300

    Add players on grid

--- core/state-manager.js
+++ core/state-manager.js
@@ -3,6 +3,22 @@ const _state = {
         google: 12,
         players: [10, 11]
     },
+    positions: {
+        google: {
+            x: 0,
+            y: 3
+        },
+        players: [
+            {
+                x: 2,
+                y: 2
+            },
+            {
+                x: 3,
+                y: 3
+            },
+        ]
+    },
     settings: {
         gridSize: {
             rowsCount: 4,
@@ -11,6 +27,19 @@ const _state = {
     }
 }
 
+//Interface
+
+function _getIndexByPlayerNumber(playerNumber) {
+    const playerIndex = playerNumber -1;
+    if (playerIndex < 0 || playerIndex > _state.points.players.length - 1) {
+        throw new Error('Incorrect player number');
+    }
+
+    return playerIndex;
+}
+
+
+//Public function
 export async function getGooglePoints() {
     return _state.points.google;
 }
@@ -19,16 +48,26 @@ export async function getGooglePoints() {
  * @param {number} playerNumber - one-based index of player
  * @return {Promise<number>} number of points
  * */
-
 export async function getPlayerPoints(playerNumber) {
-    const playerIndex = playerNumber -1;
-    if (playerIndex < 0 || playerIndex > _state.points.players.length - 1) {
-        throw new Error('Incorrect player number');
-    }
+    const playerIndex = _getIndexByPlayerNumber(playerNumber);
 
     return _state.points.players[playerIndex];
 }
 
 export async function getGridSize() {
     return {..._state.settings.gridSize}
+}
+
+export async function getGooglePosition(){
+    return {..._state.positions.google}
+}
+
+/**
+ * @param {number} playerNumber - one-based index of player
+ * @return {Promise<Object>} number of points
+ * */
+export async function getPlayerNumberPosition(playerNumber){
+    const playerIndex = _getIndexByPlayerNumber(playerNumber);
+
+    return {..._state.positions.players[playerIndex]}
 }
--- frontend/components/grid/cell/Cell.component.js
+++ frontend/components/grid/cell/Cell.component.js
@@ -1,3 +1,7 @@
+import {getGooglePosition, getPlayerNumberPosition} from "../../../../core/state-manager.js";
+import {GoogleComponent} from "../../common/google/Google.component.js";
+import {PlayerComponent} from "../../common/player/Player.component.js";
+
 export function CellComponent(x, y ) {
     const element = document.createElement('td');
     render(element, x, y)
@@ -6,5 +10,20 @@ export function CellComponent(x, y ) {
 }
 
 async function render(element, x, y){
-    element.append(`${y}, ${x}`);
+
+    const googlePosition = await getGooglePosition();
+    const player1Position = await getPlayerNumberPosition(1);
+    const player2Position = await getPlayerNumberPosition(2);
+
+    if (googlePosition.x === x && googlePosition.y === y) {
+        element.append(GoogleComponent().element);
+    }
+
+    if (player1Position.x === x && player1Position.y === y) {
+        element.append(PlayerComponent(1).element);
+    }
+
+    if (player2Position.x === x && player2Position.y === y) {
+        element.append(PlayerComponent(2).element);
+    }
 }
