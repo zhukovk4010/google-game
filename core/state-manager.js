c0180455d80306f03143f1c5378843bfbdac9663
kirill <kirill@enchantaffiliates.com>
2024-06-13 18:46:16 +0300

    add rerender and function for change google icon position

--- core/state-manager.js
+++ core/state-manager.js
@@ -21,11 +21,74 @@ const _state = {
     },
     settings: {
         gridSize: {
-            rowsCount: 4,
-            columnCount: 4
+            rowsCount: 2,
+            columnCount: 2
         }
     }
 }
+
+//Observer
+
+let _observers = []
+
+export function subscribe(observer) {
+    _observers.push(observer);
+}
+
+export function unsubscribe(observer) {
+    _observers = _observers.filter(o => o !== observer);
+}
+
+function _notifyObservers() {
+    _observers.forEach((o) => {
+        try {
+            o();
+        } catch (err) {
+            console.error(err);
+        }
+    });
+}
+
+function _generateNewNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function _jumpGoogleToNewPosition() {
+    const newPosition = {..._state.positions.google};
+
+    newPosition.x = _generateNewNumber(0, _state.settings.gridSize.columnCount - 1);
+    newPosition.y = _generateNewNumber(0, _state.settings.gridSize.rowsCount - 1);
+
+    const isNewPositionMatchWithCurrentGooglePosition =
+        newPosition.x === _state.positions.google.x &&
+        newPosition.y === _state.positions.google.y;
+
+    const isNewPositionMatchWithCurrentPlayer1Position =
+        newPosition.x === _state.positions.players[0].x &&
+        newPosition.y === _state.positions.players[0].y;
+
+    const isNewPositionMatchWithCurrentPlayer2Position =
+        newPosition.x === _state.positions.players[1].x &&
+        newPosition.y === _state.positions.players[1].y
+
+    if (
+        isNewPositionMatchWithCurrentGooglePosition ||
+        isNewPositionMatchWithCurrentPlayer1Position ||
+        isNewPositionMatchWithCurrentPlayer2Position
+    ) {
+        _jumpGoogleToNewPosition();
+    } else {
+        _state.positions.google = newPosition;
+    }
+}
+
+setInterval(() => {
+    _jumpGoogleToNewPosition();
+    _state.points.google++;
+    _notifyObservers();
+}, 1000);
 
 //Interface
 
--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,17 @@
 import {AppComponent} from "./components/App.component.js";
+import {subscribe} from "../core/state-manager.js";
 
 const rootElement = document.getElementById('root');
 
-const appComponent = AppComponent();
+function renderApp() {
+    rootElement.innerHTML = '';
 
-rootElement.append(appComponent.element);
+    const appComponent = AppComponent();
+
+    rootElement.append(appComponent.element);
+}
+
+renderApp();
+
+
+subscribe(renderApp);
